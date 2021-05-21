30c084c991ee8aca20965a7c255fd52e052e6384
goddoro <dongsagn1222@daum.net>
2021-05-21 18:59:00 +0900

    ENH : Hello world

--- app/src/main/java/com/goddoro/butcommit/MainActivity.kt
+++ app/src/main/java/com/goddoro/butcommit/MainActivity.kt
@@ -2,10 +2,30 @@ package com.goddoro.butcommit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.goddoro.butcommit.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityMainBinding
+
+    private val mViewModel : MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+
+        mBinding.lifecycleOwner = this
+        mBinding.vm = mViewModel
+
+        setContentView(mBinding.root)
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+
+
     }
 }
